9c8539de5f8d52adca6fa9cb8cd92cf8824032b2
Bjorn Van Craenenbroeck <bjorn.van.craenenbroeck@student.ehb.be>
2024-01-30 09:42:22 +0100

    push

--- Website/script.js
+++ Website/script.js
@@ -8,6 +8,17 @@ hamMenu.addEventListener("click", () => {
 })
 
 
+/* function openForm() {
+    document.getElementById("infodag").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("infodag").style.display = "none";
+} */
+const closePopUp = document.getElementById("close_pop_up");
+closePopUp.addEventListener("click", () =>{
+    document.getElementById("pop_up").style.display = "none";
+});
 
 
 
