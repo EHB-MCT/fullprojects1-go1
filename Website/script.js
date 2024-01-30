10b0f09bb6401d82f20542e72f5c8c0130727305
warre tuybens <warre.tuybens@student.ehb.be>
2024-01-30 10:52:28 +0100

    navbar

--- Website/script.js
+++ Website/script.js
@@ -1,14 +1,17 @@
 const hamMenu = document.querySelector(".ham-menu");
-
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
-})
+});
 
+const offScreenCat = document.querySelectorAll(".off-screen-cat");
+const subMenu = document.querySelectorAll(".submenu");
 
-
-
-
-
+offScreenCat.forEach((cat, index) => {
+    cat.addEventListener("click", () => {
+        subMenu[index].classList.toggle("active");
+        cat.classList.toggle("active");
+    });
+});
