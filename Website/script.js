15e07336e8bf863be13992404d84aaa491894903
warre tuybens <warre.tuybens@student.ehb.be>
2024-01-30 11:39:55 +0100

    sidebar rework

--- Website/script.js
+++ Website/script.js
@@ -1,11 +1,10 @@
 const hamMenu = document.querySelector(".ham-menu");
-const offScreenMenu = document.querySelector(".off-screen-menu");
+const offScreenMenu = document.querySelector(".sidebar");
 
 hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
-})
-
+	hamMenu.classList.toggle("active");
+	offScreenMenu.classList.toggle("active");
+});
 
 /* function openForm() {
     document.getElementById("infodag").style.display = "block";
@@ -15,18 +14,6 @@ function closeForm() {
     document.getElementById("infodag").style.display = "none";
 } */
 const closePopUp = document.getElementById("close_pop_up");
-closePopUp.addEventListener("click", () =>{
-    document.getElementById("pop_up").style.display = "none";
-});
-
-
-
-const offScreenCat = document.querySelectorAll(".off-screen-cat");
-const subMenu = document.querySelectorAll(".submenu");
-
-offScreenCat.forEach((cat, index) => {
-    cat.addEventListener("click", () => {
-        subMenu[index].classList.toggle("active");
-        cat.classList.toggle("active");
-    });
+closePopUp.addEventListener("click", () => {
+	document.getElementById("pop_up").style.display = "none";
 });
